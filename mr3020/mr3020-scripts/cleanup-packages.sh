cde5bc7bdc51fd36d9333218ac89f1cf15c3614f
gardners <paul@servalproject.org>
2013-11-11 14:32:17 +1030

    fix bug with setting ssh authorised key. improve cleanup code so that repeated running of the 2.* script can work (some issues remain, but situation is much improved)

--- mr3020/mr3020-scripts/cleanup-packages.sh
+++ mr3020/mr3020-scripts/cleanup-packages.sh
@@ -1,2 +1,6 @@
+rm /tmp/\*.ipk
 cd /overlay/usr/lib/opkg
-opkg remove `egrep "Package:| 1315" status | cut -f2 -d" " | awk '{ if (1*$1>100000) print n; n=$1; }'`
+opkg remove --force-removal-of-dependent-packages kmod-fs-ext4 
+opkg remove --force-removal-of-essential-packages libpthread
+rm /overlay/usr/lib/modules/*/ext*.ko /overlay/usr/sbin/e2fsck
+opkg remove --force-removal-of-dependent-packages `egrep "Package:| 1315" status | cut -f2 -d" " | awk '{ if (1*$1>100000) print n; n=$1; }'`
