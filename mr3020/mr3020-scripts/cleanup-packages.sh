933a6ae967c366a2390302ac8a7898f6c2a990ad
gardners <paul@servalproject.org>
2013-11-20 12:33:32 +1030

    work on mounting filesystems on mesh extender in saner way, and try fscking /serval-var.  Unfortunately fsck doesn't fit on internal flash, so we can't fsck /serval easily. also further tweaking (in vain) of cleanup-packages to clearout enough of /overlay to allow reinstallation without running firstboot from failsafe mode inbetween.

--- mr3020/mr3020-scripts/cleanup-packages.sh
+++ mr3020/mr3020-scripts/cleanup-packages.sh
@@ -4,7 +4,8 @@ opkg remove --force-removal-of-dependent-packages kmod-fs-ext4
 opkg remove --force-removal-of-essential-packages libpthread
 rm /overlay/usr/lib/modules/*/ext*.ko /overlay/usr/sbin/e2fsck
 opkg remove --force-removal-of-dependent-packages `egrep "Package:| 1315" status | cut -f2 -d" " | awk '{ if (1*$1>100000) print n; n=$1; }'`
-#cd /usr
-#rm -fr bin sbin lib
+# Clear out stray files in /overlay (deleting them from /usr to really free the space)
+cd / ; rm `cd /overlay ; find . | egrep "usr/lib|usr/sbin|lib|sbin|usr/share"`
+# Show what is left
 df -h
 find /overlay 
